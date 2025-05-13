c2b91965200908f724f06cb41789c11705920ce5
EmiliRaeder <raederemili@gmail.com>
2025-05-13 15:06:24 -0500

    FIX DUPLICATE SITES

--- app.js
+++ app.js
@@ -1,22 +1,3 @@
-// This is a simple JavaScript code to toggle between light and dark themes
-// when a button is clicked. It uses the classList API to add or remove classes;
-
-const switcher = document.querySelector('.btn');
-
-switcher.addEventListener('click', function() {
-    document.body.classList.toggle('light-theme');
-    document.body.classList.toggle('dark-theme');
-
-    const className = document.body.className;
-    if(className == "light-theme") {
-        this.textContent = "Dark";
-    } else {
-        this.textContent = "Light";
-    }
-    console.log('current class name: ' + className);
-});
-// This is a simple JavaScript code to toggle between light and dark themes
-// when a button is clicked. It uses the classList API to add or remove classes;
 
 const switcher = document.querySelector('.btn');
 
